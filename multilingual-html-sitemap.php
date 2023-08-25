0586a94c31ea36ddc0fd7c70b4cd0088b8a4810f
Shalin Shah <sahilshh723@gmail.com>
2023-08-25 21:10:28 +0530

    Plugin: Added Basic Plugin Files.

---
+++ multilingual-html-sitemap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Multilingual HTML Sitemap
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          SH4LIN
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     multilingual-html-sitemap
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Multilingual_Html_Sitemap
+ */
+
+// Your code starts here.
